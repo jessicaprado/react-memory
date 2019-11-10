96168d45159ece003077c0d904429cfbf7a7b718
Jessica Prado <jessica.marie.prado@gmail.com>
2019-11-10 13:50:39 -0500

    typescript and sass

---
+++ src/Components/Card/Card.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+class Card extends React.Component {
+    render() {
+        return (
+            <div className="card">
+                <div className="background-image"></div>
+            </div>
+        )
+    }
+}
+
+export default Card
---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <nav></nav>
+        )
+    }
+}
+
+export default Navbar;
